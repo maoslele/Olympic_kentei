516a0bfd6b92a2d14461b61509609556c1302f5d
maoslele <m01.sgwk@gmail.com>
2020-10-01 00:31:45 +0900

    インデントを補正

--- quiz_action.rb
+++ quiz_action.rb
@@ -13,7 +13,7 @@ class Quiz
   attr_reader :selected_quiz
   attr_accessor :grade, :score, :result
 
-    def initialize
+  def initialize
     @selected_quiz = []
     @grade = 0
     @score = 0
@@ -123,8 +123,8 @@ class Quiz
   def check_ans(ans, shuffle_ans, quiz_ans)
     ans_num = (ans == "A") ? 0 : (ans == "B" ) ? 1 : 2
     if shuffle_ans[ans_num] == quiz_ans[0]
-       puts "正解です。"
-       @score += 1
+      puts "正解です。"
+      @score += 1
     else
       puts "不正解です。正解は#{quiz_ans[0]}です。"
     end
